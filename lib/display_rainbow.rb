fcc81ca5fc310483093b67f85c28a31f508811be
Jawann <jawann@gmail.com>
2017-10-22 00:45:48 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,5 +1,5 @@
 def display_rainbow (rainbow)
-  rainbow[0...-1].each {|x| p "#{rainbow[x].first.uppercase}: #{rainbow[x]}, "
+  rainbow[0...-1].each {|x| p "#{rainbow[x].first.uppercase}: #{rainbow[x]}, "}
   puts "#{rainbow.last.first.uppercase}: #{rainbow.last}"
   rainbow
 end
