ad3ec6cc8f63202dc580c00c78f1b15759a021c3
Jawann <jawann@gmail.com>
2017-10-22 01:16:34 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -2,5 +2,4 @@ def display_rainbow (rainbow = ['red', 'orange', 'yellow', 'green', 'blue', 'ind
   rain_arr = []
   rainbow.each {|x| rain_arr << "#{x[0].upcase}: #{x}"}
   puts rain_arr.join(", ")
-  rainbow
 end
